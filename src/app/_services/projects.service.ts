73899e7c4bf06670e8bf73967c273f2c7a3ce0ce
Bruno Janeiro <janeiro.bruno23@gmail.com>
2024-01-03 20:38:46 +0100

    final

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -10,11 +10,11 @@ export class ProjectsService {
   projects: Project[] = [
 
     {id: 0,
-    name: "Angular Portfolio Website",
-    summary: "Test Description",
-    description: "",
-    projectLink: "",
-    pictures: ["../../assets/image1.png", "../../assets/image2.png", "../../assets/image3.png"],
+    name: " Portfolio ",
+    summary: "🎓",
+    description: "Individual project with 4 main pages, with a brief introduction about the programmer; list of projects, link to CV & contacts.",
+    projectLink: "https://github.com/bjaneiro90/portfolio",
+    pictures: ["../../assets/portefolio_1.PNG", "../../assets/portefolio_2.PNG", "../../assets/portefolio_3.PNG"],
     tag1: ["../../assets/angular-1-32.png"],
     tag2: ["../../assets/javascript-3-32.png"],
     tag3: ["../../assets/bootstrap-2-32.png"],
@@ -22,26 +22,46 @@ export class ProjectsService {
     },
 
    {id: 1,
-    name: "Drum Instrument",
-    summary: "Test Description",
-    description: "",
-    projectLink: "",
-    pictures: ["../../assets/image1.png", "../../assets/image2.png", "../../assets/image3.png"],
+    name: "Drum_Blaster",
+    summary: "🥁",
+    description: "The drum music instrument was the first group project created during the course, where the user could play and record music. We used events to create dynamics between the instrument's compounds and the user's keyboard, and CSS to create the aesthetics whenever a component was played.",
+    projectLink: "https://github.com/tebanet/proyecto-bateria",
+    pictures: ["../../assets/Drum_1.PNG"],
     tag1: ["../../assets/4202020_css3_html_logo_social_social media_icon.png"],
     tag2: ["../../assets/javascript-3-32.png"],
     tag3: ["../../assets/html5-12-32.png"]
     },
 
     {id: 3,
-      name: "API for retro products",
-      summary: "Test Description",
-      description: "",
-      projectLink: "",
-      pictures: ["../../assets/image1.png", "../../assets/image2.png", "../../assets/image3.png"],
+      name: "Retro Shop (Back-End)",
+      summary: "🖥",
+      description: "The online vintage product store backend project was the second project of the course, where we had to create a database of requests that responded to the designated routes. The main objective of this app was to develop a online process for selling, purchasing and rating vintage products through the creation of the registration and login process with token and validation with joi; production of a list of notes filtered by price, category, name and location; and the creation of a user experience evaluation system. ",
+      projectLink: "https://github.com/tebanet/retrotech-shop",
+      pictures: ["../../assets/retro_1.PNG", "../../assets/retro_2.PNG", "../../assets/retro_3.PNG"],
+      tag1: ["../../assets/mysql.png"],
+      tag2: ["../../assets/javascript-3-32.png"],
+      tag3: ["../../assets/nodejs-performance-platform-32.png"]
+    },
+    {id: 4,
+      name: "Noted App (Front-End)",
+      summary: "📝",
+      description: "The online notebook project was my third, individual and last project during the bootcamp. The main objective of this application was to create an attractive look and implement a clean and objective design, capable of being maintained and scalable through the creation of the registration and login process with token and validation with joi; production of a list of notes for each user; and creation of a system for posting, editing and deleting posts with images.",
+      projectLink: "https://github.com/bjaneiro90/appDeNotas",
+      pictures: ["../../assets/noted_1.PNG", "../../assets/noted_2.PNG", "../../assets/noted_3.PNG"],
       tag1: ["../../assets/react-32.png"],
       tag2: ["../../assets/javascript-3-32.png"],
       tag3: ["../../assets/nodejs-performance-platform-32.png"]
     },
+    {id: 3,
+      name: "Noted App (Back-End)",
+      summary: "🖥",
+      description: "The online notebook project was my third, individual and last project during the bootcamp. The development of the API included not only the creation of a database, but also requests and responses capable of establishing a connection with the client in a secure way. The main objectives of this project were to create the registration and login process with token and validation with joi; create a list of notes; and post, edit and delete notes with images.",
+      projectLink: "https://github.com/tebanet/retrotech-shop",
+      pictures: ["../../assets/retro_1.PNG", "../../assets/retro_2.PNG", "../../assets/retro_3.PNG"],
+      tag1: ["../../assets/mysql.png"],
+      tag2: ["../../assets/javascript-3-32.png"],
+      tag3: ["../../assets/nodejs-performance-platform-32.png"]
+    }
     ];
 
 
